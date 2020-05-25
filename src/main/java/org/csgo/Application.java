1ab14142c33929ee67b1e979c566124828c24963
Jeffrey <jeffrey.mckay@libertymutual.com>
2020-05-25 11:12:10 -0400

    Adding method to fetch basic user info... just coming back as string currently

--- src/main/java/org/csgo/Application.java
+++ src/main/java/org/csgo/Application.java
@@ -3,10 +3,11 @@ package org.csgo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
 
--- src/main/java/org/csgo/service/CustomUserDetailsService.java
+++ src/main/java/org/csgo/service/CustomUserDetailsService.java
@@ -6,40 +6,57 @@
 package org.csgo.service;
 
 import org.csgo.model.User;
-import org.csgo.repository.SteamUserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.openid.OpenIDAuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Service
 public class CustomUserDetailsService implements AuthenticationUserDetailsService<OpenIDAuthenticationToken> {
 
     private static final Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
-    @Autowired
-    SteamUserRepository steamUserRepository;
+    //@Autowired
+    //SteamUserRepository steamUserRepository;
 
+    String web_key = "88721AB6DB5DA8E1384A8DB9B493690C";
 
     @Override
     public UserDetails loadUserDetails(OpenIDAuthenticationToken token) throws UsernameNotFoundException {
 
         log.info("Loading user details ...");
-
         User user = new User();
 
         try {
             //You can find user by the token...
             log.info("Finding user by token " + token.getName());
+            findSteamUserInfo(token.getName());
 
             //steamUserRepository.save()
         } catch (RuntimeException e) {
             log.error("Runtime excetion");
         }
         return user;
+    }
+
+    public void findSteamUserInfo(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.valueOf("text/html; charset=UTF-8"));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + web_key
+                + "&steamids=" + token;
+        System.out.println(url);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println(responseEntity.getBody().toString());
+
     }
 }
